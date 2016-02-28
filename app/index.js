e2c56ef5a6ae79c1efd16bbe62661cfedb43be12
natac13 <sean.campbell13@gmail.com>
2016-02-27 19:10:17 -0500

    chore(Move): Setup functions
    I have moved up the injectTapPlugin and installDevTools to the very top of the entry file.

--- app/index.js
+++ app/index.js
@@ -1,3 +1,9 @@
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
+import Immutable from 'immutable'
+import installDevTools from 'immutable-devtools'
+installDevTools(Immutable)
 import React from 'react'
 import { render } from 'react-dom'
 // import Router            from 'react-router';
@@ -11,13 +17,6 @@ import App from './containers/App/'
 
 import configureStore from './store/configureStore'
 const store = configureStore(Map())
-
-import injectTapEventPlugin from 'react-tap-event-plugin'
-injectTapEventPlugin()
-
-import Immutable from 'immutable'
-import installDevTools from 'immutable-devtools'
-installDevTools(Immutable)
 
 const rootElement = document.getElementById('root')
 // import { history } from './store/configureStore';
